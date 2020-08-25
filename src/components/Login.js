19d0b7cb7edb1309e92b63571c9d5769bdff5d36
Erica Girges <ericafiorenza@gmail.com>
2020-08-24 17:55:39 -0700

    deploy forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import * as yup from "yup";
 import loginSchema from "./validation/loginSchema";
-import styled from "styled-components";
+// import styled from "styled-components";
 
 //set initial form shape and values
 const initialLoginValues = {
